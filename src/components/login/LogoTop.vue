f28b81f304984a17b5d1aafbac549124f35144dc
Genndiy <gsabinin@icloud.com>
2022-01-20 16:22:00 +0300

    fixed

---
+++ src/components/login/LogoTop.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="LogoTop q-my-md">
+  <img
+      width="110"
+      height="110"
+      class="q-mb-sm"
+      src="../../assets/img/1.svg"
+      alt="logo">
+    <h5 class="no-margin text-center">QR-Menu</h5>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: "LogoTop",
+    data() {
+        return {
+
+        }
+    },
+    created() {},
+    mounted() {},
+    components: {},
+    computed: {},
+    methods: {},
+})
+</script>
+
+<style lang="scss" scoped>
+.LogoTop {
+  h5 {
+    font-family: 'Raleway',sans-serif;
+    color: #ffffff;
+    font-weight: bold;
+  }
+}
+</style>
---
+++ src/components/login/WaiterCard.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="WaiterCard">
+    <router-link class="row items-center no-wrap bg-white q-pa-md" :to="'/restaurant/login/' + waiter.id">
+      <img width="65" height="65" src="https://toppng.com/uploads/preview/instagram-default-profile-picture-11562973083brycehrmyv.png" alt="waiter">
+      <span class="waiter-name q-ml-md">{{waiter.name}}</span>
+    </router-link>
+</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: "WaiterCard",
+    props: ['waiter'],
+    data() {
+        return {
+
+        }
+    },
+    created() {},
+    mounted() {},
+    components: {},
+    computed: {},
+    methods: {},
+})
+</script>
+
+<style lang="scss" scoped>
+.WaiterCard {
+  padding: 0 10px 10px 0;
+  a {
+    text-decoration: none;
+    border-radius: 13px;
+    img {
+      border-radius:50%;
+    }
+    .waiter-name {
+      font-family: 'Raleway',sans-serif;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 20px;
+      line-height: 130%;
+      color: #1E1E1E;
+      height: 26px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+  }
+}
+</style>
