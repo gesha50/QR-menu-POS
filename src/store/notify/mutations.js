8f5f8dcdf930138e9ce58a31739fde7a24c6cd78
Ziyovuddin <ziyovuddinmahmudov41@gmail.com>
2022-01-27 09:57:18 +0500

    Cart and Notifications

--- src/store/notify/mutations.js
+++ src/store/notify/mutations.js
@@ -2,8 +2,13 @@
 export function addNotify (state, notify) {
   console.log(state.notifications)
   state.notifications = [...state.notifications, notify]
-  localStorage.setItem('notify', JSON.stringify(state.notifications))
+  // localStorage.setItem('notify', JSON.stringify(state.notifications))
 }
+
+// export function removeNotify (state, index) {
+//   state.notifications.splice(indexOf(index), 1);
+//   localStorage.setItem('notify', JSON.stringify(state.notifications))
+// }
 
 export function seeAllNotify(state) {
   console.log(state.notifications)
