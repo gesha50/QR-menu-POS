1773015e1d80a35c5ecd6e9981ebb00b75ab2cb0
Alisher <alisheroxunov1383@gmail.com>
2022-02-14 15:45:24 +0500

    connection error page built

---
+++ src/pages/NoConnection.vue
@@ -0,0 +1,43 @@
+<template>
+        <q-page>
+            <div class="container q-pt-lg">
+                <div class="logo q-mb-lg">
+                    <q-img src="../assets/img/logo_login.svg"></q-img>
+                </div>
+                <div class="q-pa-md connection-error-img">
+                    <q-img
+                        src="../assets/img/no-connection.jpg"
+                    >
+                    </q-img>
+                </div>
+
+                <div class="message q-mt-md">
+                    <p>Нет подключения к интернету</p>
+                </div>
+            </div>
+        </q-page>
+</template>
+<script>
+
+</script>
+<style lang="scss">
+.container{
+  width: 80%;
+  margin: 0 auto;
+  background-color: #fff;
+    .logo{
+        width: 85px;
+        margin: 0 auto;
+    }
+    .connection-error-img{
+        width: 42%;
+        margin: 0 auto;
+    }
+    .message{
+        text-align: center;
+        font-weight: bold;
+        font-size: 19px;
+        color: #1E1E1E;
+    }
+}
+</style>
