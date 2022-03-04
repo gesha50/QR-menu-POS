db6784dfc76b175af3b71fb68c2323d179229b91
Genndiy <gsabinin@icloud.com>
2022-03-04 10:44:34 +0300

    fixed

--- src/assets/functions/getVariant.js
+++ src/assets/functions/getVariant.js
@@ -2,19 +2,17 @@ import {isEqual} from "assets/functions/isEqual";
 
 export function getVariant(item) {
   let curVariant = {}
-  let variant_id = null
+  let variant = null
 
   item.options.forEach(op=>{
     curVariant[op.id] = op.value.toLowerCase()
   })
 
   item.variants.forEach(v=>{
-    if (variant_id) {
-      return
-    }
+    if (variant) { return }
     if (isEqual(JSON.parse(v.options), curVariant)){
-      variant_id = v.id
+      variant = v
     }
   })
-  return variant_id
+  return variant
 }
